4b083c9cf20154ccad0dbc3687a29d37171c2914
Lei Qian <lei.qian@duke.edu>
2017-04-18 18:41:00 -0400

    Sourced loadData.R in armed robbery file

--- armedRobbery.R
+++ armedRobbery.R
@@ -2,7 +2,7 @@ library(dplyr)
 library(data.table)
 library(sf)
 
-load("/home/grad/rkm22/sta644/data/fullCrimeData.Rdata")
+source("loadData.R")
 
 gunTypes = c("ARMED: HANDGUN", "ARMED: OTHER FIREARM", "ATTEMPT: ARMED-HANDGUN", "ATTEMPT: ARMED-OTHER FIREARM")
 dfArmed = df %>% filter(Description %in% gunTypes)
