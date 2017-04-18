20f2720a1abab2cba49c3bf0e8ffee5fc23d9d0a
Lei Qian <lei.qian@duke.edu>
2017-04-18 17:38:06 -0400

    Also load data

---
+++ loadDataLei.R
@@ -0,0 +1,8 @@
+library(data.table)
+library(dplyr)
+#library(jsonlite)
+#test2 = fromJSON("https://data.cityofchicago.org/resource/6zsd-86xi.json")
+# df = fread("crime_data.csv")
+# save(df, file = "crime_data.Rdata")
+
+load("/home/grad/lsq3/spatio_temp_proj/crime_data.Rdata")
