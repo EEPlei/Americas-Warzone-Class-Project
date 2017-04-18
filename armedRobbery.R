93d5da8a0d8494da0ad8db75513c820134489ee8
Reuben McCreanor <reuben.mccreanor@duke.edu>
2017-04-18 18:37:11 -0400

    Meghan you are my hero

--- armedRobbery.R
+++ armedRobbery.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(data.table)
+library(sf)
 
 load("/home/grad/rkm22/sta644/data/fullCrimeData.Rdata")
 
@@ -7,6 +8,9 @@ gunTypes = c("ARMED: HANDGUN", "ARMED: OTHER FIREARM", "ATTEMPT: ARMED-HANDGUN",
 dfArmed = df %>% filter(Description %in% gunTypes)
 
 head(dfArmed)
+
+areas = st_read('/home/grad/rkm22/sta644/spatio_temp_proj/shapeData/CommAreas.shp', 
+                quiet=TRUE, stringsAsFactors=TRUE)
 
 
 
