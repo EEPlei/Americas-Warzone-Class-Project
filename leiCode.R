308013ba76f5095286e8f0802ee20cb681d1da7c
Lei Qian <lei.qian@duke.edu>
2017-04-18 18:11:45 -0400

    No Merge Conflicts!

---
+++ leiCode.R
@@ -0,0 +1,8 @@
+source('loadData.R')
+
+dupes = df %>% "["(.,df %>% select(`Case Number`) %>%
+                     duplicated() %>% 
+                     which()) %>% 
+  select(`Case Number`) %>% 
+  unlist()
+df %>% filter(`Case Number` %in% dupes) %>% arrange(`Case Number`) %>% View()
