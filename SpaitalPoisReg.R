bb8cad03522425d73c285b1032c849cdd027fa0a
Lei Qian <lei.qian@duke.edu>
2017-05-01 13:50:01 -0400

    Added Residual + Prediction Plots

--- SpaitalPoisReg.R
+++ SpaitalPoisReg.R
@@ -6,6 +6,7 @@ NewPackages <- listOfPackages[!(listOfPackages %in% installed.packages()[,"Packa
 if(length(NewPackages)>0) {install.packages(NewPackages,repos="http://cran.rstudio.com/")}
 
 library(raster)
+library(glmnet)
 library(magrittr)
 library(modelr)
 library(ggplot2)
@@ -43,6 +44,7 @@ D = diag(rowSums(W))
 
 X = model.matrix(~scale(dfFull %>% select(-contains("community area"), -contains("geog"), -contains("armedrobbery"))))
 Xvars = dfFull %>% select(-contains("community area"), -contains("geog"), -contains("armedrobbery")) %>% colnames()
+colnames(X) = c("Intercept", Xvars)
 log_offset = log(dfFull$ArmedRobbery)
 y = dfFull$ArmedRobbery
 morans_I(y = y, w = D %*% W)
@@ -114,3 +116,48 @@ grid.arrange(
 
 # RMSE 
 armed_robbery_pred$resid %>% .^2 %>% mean() %>% sqrt()
+
+# lasso without spatial random effect #
+
+uncoolFit = glmnet(x = X[,-1], y = y , family = "poisson", alpha = 1)
+plot(uncoolFit)
+coef(uncoolFit, s= 1)
+
+
+uncool_pred = areas %>% 
+  mutate(obs_pred = predict(uncoolFit, X[,-1],type = "response", s = 1) %>% as.vector(),
+         resid = (y - predict(uncoolFit, X[,-1],type = "response", s = 1)) %>% as.vector())
+
+grid.arrange(
+  ggplot(uncool_pred) +
+    geom_sf(aes(fill=obs_pred), color=NA) + 
+    labs(title="Predicted Cases",fill=""),
+  ggplot(uncool_pred) +
+    geom_sf(aes(fill=resid), color=NA) +
+    labs(title="Residuals",fill=""),
+  ncol=2
+)
+
+# RMSE 
+uncool_pred$resid %>% .^2 %>% mean() %>% sqrt()
+
+
+comb_pred = rbind(uncool_pred, armed_robbery_pred)
+comb_pred = comb_pred %>% mutate(Regression = rep(c("Penalized", "Spatial Penalized"), each = 77))
+pdf("results.pdf", width = 9, height = 9)
+grid.arrange(
+  ggplot(comb_pred) +
+    geom_sf(aes(fill=obs_pred), color=NA) + 
+    labs(title="Predicted Cases",fill="") + 
+    facet_grid(Regression ~ .) + 
+    scale_fill_gradient2(low = "purple", mid = "blue", high = "green", 
+                        na.value = "grey50", guide = "colourbar", midpoint = 5000),
+  ggplot(comb_pred) +
+    geom_sf(aes(fill=resid), color=NA) + 
+    labs(title="Residuals",fill="") + 
+    facet_grid(Regression ~ .) + 
+    scale_fill_gradient2(low = "purple", mid = "blue",high = "green", 
+                        na.value = "grey50", guide = "colourbar", midpoint = 0),
+  ncol=2
+)
+dev.off()
