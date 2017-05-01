c1706f82f75105a9d45dab4d80db6c0bf1d17800
Lei Qian <lei.qian@duke.edu>
2017-05-01 14:39:49 -0400

    Data Source PDF

--- loadData.R
+++ loadData.R
@@ -3,7 +3,11 @@ library(sf)
 library(lubridate)
 library(dplyr)
 library(readxl)
+library(gridExtra)
 
 
 load("/home/grad/rkm22/sta644/data/fullCrimeData.Rdata")
 
+pdf("totaldata.pdf", width = 33, height = 5.5)
+grid.table(df[1:10,])
+dev.off()
